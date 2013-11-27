71b13368b128f2ec7dbe3be44fe4afb2f0d6befc
Daniel Gracia <duodigdaniel@gmail.com>
2013-11-27 11:37:40 -0200

    Wrap field, random game, some refactoring, etc

---
+++ gameoflife/play.js
@@ -0,0 +1,47 @@
+(function() {
+	var update = document.getElementById("update");
+	var next = document.getElementById("next");
+	var stop = document.getElementById("stop");
+	var interval = document.getElementById("genInterval");
+	var canvas = document.getElementById("canvas");
+	var game = new Life(canvas, {
+		width: 20,
+		height: 20
+	});
+
+	var rndParams = {
+		x: {
+			min: 0,
+			max: game.area.width - 1
+		},
+		y: {
+			min: 0,
+			max: game.area.height - 1
+		}
+	};
+
+	LU.repeat((game.area.size/5) | 0, function() {
+		return LU.pair(chance.natural(rndParams.x), chance.natural(rndParams.y));
+	}).forEach(function(e) {
+		game.area.toggle(e.fst, e.snd);
+	});
+
+	var timer = new LU.Timer(function() {
+		game.area.next();
+	}, 0);
+
+	update.addEventListener('click', function () {
+		timer.interval = interval.value*1000;
+		timer.restart();
+	});
+
+	next.addEventListener('click', function () {
+		game.area.next();
+	});
+
+	stop.addEventListener('click', function() {
+		timer.stop();
+	});
+
+	var loop = game.start();
+})();
